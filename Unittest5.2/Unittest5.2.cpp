40ed4aed0fea407fe88ba96fe59297d6ed6c7160
StanislavSorochak <75738611+StanislavSorochak@users.noreply.github.com>
2020-12-15 21:03:28 +0200

    first master commit

---
+++ Unittest5.2/Unittest5.2.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba5.3/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Unittest52
+{
+	TEST_CLASS(Unittest52)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			//int f=0;
+			t = f(2);
+			Assert::AreEqual(t, 0.717258, 00591505.);
+
+		}
+	};
+}
